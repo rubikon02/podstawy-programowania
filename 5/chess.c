b893f80b3cd7c47956f3a8755c0ff1224671ab23
rubikon02 <rubikon02@gmail.com>
2022-11-17 23:37:44 +0100

    reduce nested ifs

--- 5/chess.c
+++ 5/chess.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <time.h>
 
 #define WIN_SCORE 1000
 #define LOSE_SCORE ( -1000 )
@@ -14,7 +15,7 @@ const int SCORES[] = {0,
 const int VECTOR_COUNTS[] = {0,
                              3, 8, 4, 4, 8, 8,
                              3, 8, 4, 4, 8, 8};
-#define DEPTH 4
+#define DEPTH 6
 #define MAX_MOVES 8
 const int MAX_VECTOR_LENGTHS[] = {0,
                                   1, 1, BOARD_SIZE, BOARD_SIZE, BOARD_SIZE, 1,
@@ -76,43 +77,41 @@ void print_board(int board[BOARD_SIZE][BOARD_SIZE]) {
 }
 
 int best_move(int board[BOARD_SIZE][BOARD_SIZE], int depth, int *from_x, int *from_y, int *to_x, int *to_y) {
-    int dummy;
-    int score = get_board_score(board);
+    int dummy, score = get_board_score(board);
     if (score >= WIN_SCORE / 2 || score <= LOSE_SCORE / 2 || depth == 0) return score;
     if (depth % 2 == (DEPTH % 2)) {                   //ruch komputera
         int max_score = 100 * LOSE_SCORE;
         for (int x = 0; x < BOARD_SIZE; x++) {
             for (int y = 0; y < BOARD_SIZE; y++) {
-                if (1 <= board[x][y] && board[x][y] <= 6) {
-                    for (int vi = 0; vi < VECTOR_COUNTS[board[x][y]]; vi++) {
-                        const int dx = WX[board[x][y]][vi];
-                        const int dy = WY[board[x][y]][vi];
-                        for (int v_len = 1; v_len <= MAX_VECTOR_LENGTHS[board[x][y]]; v_len++) {
-                            const int dest_x = x + dx * v_len;
-                            const int dest_y = y + dy * v_len;
-                            if (!(0 <= dest_x && dest_x < BOARD_SIZE && 0 <= dest_y && dest_y < BOARD_SIZE)) continue;
-                            if (board[dest_x][dest_y] == 0 || board[dest_x][dest_y] > 6) {
-                                if (board[x][y] == 1 || board[x][y] == 7) {
-                                    if (dest_x != x && v_len != 1) continue;
-                                    if (dest_x != x && board[dest_x][dest_y] <= 6) continue;
-                                    if (dest_x == x && v_len == 2 && ((board[x][y] == 1 && y == 1) || (board[x][y] == 7 && y == 6))) continue;
-                                }
-                                const int dest_field_content = board[dest_x][dest_y];
-                                board[dest_x][dest_y] = board[x][y];
-                                board[x][y] = 0;
-                                score = best_move(board, depth - 1, &dummy, &dummy, &dummy, &dummy);
-                                board[x][y] = board[dest_x][dest_y];
-                                board[dest_x][dest_y] = dest_field_content;
-                                if (score > max_score) {
-                                    max_score = score;
-                                    *from_x = x;
-                                    *from_y = y;
-                                    *to_x = dest_x;
-                                    *to_y = dest_y;
-                                }
+                if (!(1 <= board[x][y] && board[x][y] <= 6)) continue;
+                for (int vi = 0; vi < VECTOR_COUNTS[board[x][y]]; vi++) {
+                    const int dx = WX[board[x][y]][vi];
+                    const int dy = WY[board[x][y]][vi];
+                    for (int v_len = 1; v_len <= MAX_VECTOR_LENGTHS[board[x][y]]; v_len++) {
+                        const int dest_x = x + dx * v_len;
+                        const int dest_y = y + dy * v_len;
+                        if (!(0 <= dest_x && dest_x < BOARD_SIZE && 0 <= dest_y && dest_y < BOARD_SIZE)) continue;
+                        if (board[dest_x][dest_y] == 0 || board[dest_x][dest_y] > 6) {
+                            if (board[x][y] == 1 || board[x][y] == 7) {
+                                if (dest_x != x && v_len != 1) continue;
+                                if (dest_x != x && board[dest_x][dest_y] <= 6) continue;
+                                if (dest_x == x && v_len == 2 && ((board[x][y] == 1 && y == 1) || (board[x][y] == 7 && y == 6))) continue;
                             }
-                            if (board[dest_x][dest_y]) break;
+                            const int dest_field_content = board[dest_x][dest_y];
+                            board[dest_x][dest_y] = board[x][y];
+                            board[x][y] = 0;
+                            score = best_move(board, depth - 1, &dummy, &dummy, &dummy, &dummy);
+                            board[x][y] = board[dest_x][dest_y];
+                            board[dest_x][dest_y] = dest_field_content;
+                            if (score > max_score) {
+                                max_score = score;
+                                *from_x = x;
+                                *from_y = y;
+                                *to_x = dest_x;
+                                *to_y = dest_y;
+                            }
                         }
+                        if (board[dest_x][dest_y]) break;
                     }
                 }
             }
@@ -188,16 +187,20 @@ int main() {
 
     for (int curr_player = 1; score != WIN_SCORE && score != LOSE_SCORE; curr_player = !curr_player) {
         if (curr_player) {
+            clock_t begin = clock();
             best_move(board, DEPTH, &from_x, &from_y, &to_x, &to_y);
+            clock_t end = clock();
+            double time_spent = (double) (end - begin) / CLOCKS_PER_SEC;
+            printf("Ruch zajal %.2fs\n", time_spent);
         } else {
             printf("Podaj pozycje:");
             scanf("%d%d%d%d", &from_x, &from_y, &to_x, &to_y);
         }
         board[to_x][to_y] = board[from_x][from_y];
         board[from_x][from_y] = ' ';
-        print_board(board);
         score = get_board_score(board);
         printf("%c[%d][%d]->[%d][%d], wynik: %d\n", PIECE_LETTERS[board[to_x][to_y]], from_x, from_y, to_x, to_y, score);
+        print_board(board);
         if (score == WIN_SCORE) printf("Przegrales\n");
         if (score == LOSE_SCORE) printf("Wygrales\n");
         printf("\n");
