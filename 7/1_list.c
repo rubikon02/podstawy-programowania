12c3884411cfafd8346fda96f62fa818a8df3511
rubikon02 <rubikon02@gmail.com>
2022-12-10 17:05:12 +0100

    Fix find and refactor

--- 7/1_list.c
+++ 7/1_list.c
@@ -4,51 +4,67 @@
 
 struct student {
     int ocena;
-    char nazwisko[10];
+    char nazwisko[100];
     struct student *nastepny;
 };
 
-void wypisz(struct student student) {
+struct student *nowy_el(char *nazwisko, int ocena) {
+    struct student *nowy = malloc(sizeof(struct student));
+    nowy->ocena = ocena;
+    strcpy(nowy->nazwisko, nazwisko);
+    nowy->nastepny = 0;
+    return nowy;
+}
+
+struct student *wczytaj_el() {
+    struct student *nowy = nowy_el("", 0);
+    printf("Podaj ocene:");
+    scanf("%d", &nowy->ocena);
+    printf("Podaj nazwisko:");
+    scanf("%s", nowy->nazwisko);
+    return nowy;
+}
+
+void wypisz_el(struct student student) {
     printf("Student %s z ocena %d\n", student.nazwisko, student.ocena);
 }
 
 void wypisz_liste(struct student *student) {
     while (student) {
-        wypisz(*student);
+        wypisz_el(*student);
         student = student->nastepny;
     }
 }
 
-void dodaj_el(struct student **pierwszy, char nazwisko[10], int ocena) {
+void dodaj_el(struct student **pierwszy, char *nazwisko, int ocena) {
     struct student *ww = *pierwszy;
-
-    struct student *nowy_student = malloc(sizeof(struct student));
-    nowy_student->ocena = ocena;
-    nowy_student->nastepny = 0;
-    strcpy(nowy_student->nazwisko, nazwisko);
+    struct student *nowy = nowy_el(nazwisko, ocena);
     if (ww == 0) {
-        *pierwszy = nowy_student;
+        *pierwszy = nowy;
     } else {
         while (ww->nastepny) ww = ww->nastepny;
-        ww->nastepny = nowy_student;
+        ww->nastepny = nowy;
     }
 }
 
-void zwolnij_liste(struct student *student) {
-    do {
-        struct student *temp = student;
-        student = student->nastepny;
-//        printf("zwalniam %s\n", temp->nazwisko);
-        free(temp);
-    } while (student);
+void zwolnij_liste(struct student **student) {
+    if (!student) return;
+    struct student *ww = *student;
+    while (ww) {
+        struct student *poprzedni = ww;
+        ww = ww->nastepny;
+//        printf("zwalniam %s\n", poprzedni->nazwisko);
+        free(poprzedni);
+    }
+    *student = 0;
 }
 
-void wstaw_el(struct student **pierwszy, int ocena) {
+void wstaw_el(struct student **pierwszy, char *nazwisko, int ocena) {
     struct student *ww = *pierwszy;
 
     struct student *nowy = malloc(sizeof(struct student));
     nowy->ocena = ocena;
-    strcpy(nowy->nazwisko, "nazwisko_");
+    strcpy(nowy->nazwisko, nazwisko);
 
     if (ww == 0 || nowy->ocena < ww->nastepny->ocena) {
         nowy->nastepny = *pierwszy;
@@ -62,13 +78,12 @@ void wstaw_el(struct student **pierwszy, int ocena) {
     }
 }
 
-struct student * znajdz_element(struct student **pierwszy, int ocena) {
-    struct student *ww = *pierwszy;
-    if (ww == 0) return 0;
-    while (ww->nastepny && ww->ocena != ocena) {
+struct student *znajdz_element(struct student *ww, int ocena) {
+    while (ww) {
+        if (ww->ocena == ocena) return ww;
         ww = ww->nastepny;
     }
-    return ww;
+    return 0;
 }
 
 void usun_element(struct student **pierwszy, int ocena) {
@@ -79,25 +94,39 @@ void usun_element(struct student **pierwszy, int ocena) {
         free(ww);
         return;
     }
-    poprzedni = ww;
-    while (ww->nastepny && ww->ocena != ocena) {
+    while (ww) {
+        if (ww->ocena == ocena) {
+            poprzedni->nastepny = ww->nastepny;
+            free(ww);
+            return;
+        }
         poprzedni = ww;
         ww = ww->nastepny;
     }
-    poprzedni->nastepny = ww->nastepny;
-    free(ww);
 }
 
 int main() {
     struct student *ws = 0;
+
+//    struct student *b = wczytaj_el();
+//    dodaj_el(&ws, b->nazwisko, b->ocena);
+
     dodaj_el(&ws, "nazwisko1", 1);
-    dodaj_el(&ws, "nazwisko2", 2);
-    dodaj_el(&ws, "nazwisko3", 5);
-    wstaw_el(&ws, 100);
-    struct student *znaleziony = znajdz_element(&ws, 1);
-    printf("%s\n", znaleziony->nazwisko);
+    dodaj_el(&ws, "nazwisko2", 4);
+    dodaj_el(&ws, "nazwisko3", 10);
+    wstaw_el(&ws, "wstawiony", 3);
     wypisz_liste(ws);
-    usun_element(&ws, 100);
+
+    struct student *znaleziony = znajdz_element(ws, 1);
+    if (znaleziony) {
+        printf("Znalazlem %s z ocena %d\n", znaleziony->nazwisko, znaleziony->ocena);
+    } else {
+        printf("Nie znalazlem takiego studenta\n");
+    }
+
+    printf("Usuwam element\n");
+    usun_element(&ws, 1);
     wypisz_liste(ws);
-    zwolnij_liste(ws);
+
+    zwolnij_liste(&ws);
 }
