b058bc5fc6ece8c8ba626c6ca1febcdaf1f17792
rubikon02 <rubikon02@gmail.com>
2022-12-20 00:17:56 +0100

    Let server listen for another connection after client disconnects

--- 9/1_chat/klient.c
+++ 9/1_chat/klient.c
@@ -7,7 +7,7 @@
 #define IP "127.0.0.23"
 
 int main() {
-    printf("Uruchamiam klienta na %s:%d\n", IP, PORT);
+    printf("Uruchamiam klienta i nawiązuję połączenie z %s:%d\n", IP, PORT);
 
     int gniazdo;
     ssize_t status;
@@ -19,25 +19,39 @@ int main() {
     ser.sin_addr.s_addr = inet_addr(IP);
 
     gniazdo = socket(AF_INET, SOCK_STREAM, 0);
-    if (gniazdo==-1) { printf("blad socket\n"); return 0; }
+    if (gniazdo == -1) {
+        printf("Błąd socket\n");
+        return 0;
+    }
 
-    status = connect(gniazdo, (struct sockaddr*)&ser, sizeof ser);
-    if (status<0) { printf("blad connect\n"); return 0; }
+    status = connect(gniazdo, (struct sockaddr *) &ser, sizeof ser);
+    if (status < 0) {
+        printf("Błąd connect\n");
+        return 0;
+    }
+
+    printf("Podłączono do serwera\n");
 
     while (1) {
-        printf("Podaj wiadomosc do wyslania:");
+        printf("Podaj wiadomosć do wysłania:");
         scanf("%s", buf);
         status = send(gniazdo, buf, strlen(buf), 0);
-        if (status < 0) { printf("blad wysylania wiadomosci\n"); return 0; }
+        if (status <= 0) {
+            printf("Błąd wysyłania wiadomosci\n");
+            return 0;
+        }
         if (strcmp(buf, "Q") == 0) break;
 
         printf("Czekam na wiadomosc...\n");
         status = recv(gniazdo, buf, (sizeof buf) - 1, 0);
-        if (status < 0) { printf("blad otrzymywania wiadomosci\n"); return 0; }
-        buf[status]='\0';
-        printf ("OTRZYMANO: #%s#\n", buf);
+        if (status <= 0) {
+            printf("Błąd otrzymywania wiadomości\n");
+            return 0;
+        }
+        buf[status] = '\0';
+        printf("OTRZYMANO: #%s#\n", buf);
         if (strcmp(buf, "Q") == 0) break;
     }
-    printf("Rozlaczam gniazdo\n");
+    printf("Rozłączam gniazdo\n");
     close(gniazdo);
 }
--- 9/1_chat/serwer.c
+++ 9/1_chat/serwer.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#include<arpa/inet.h>
-#include<string.h>
+#include <arpa/inet.h>
+#include <string.h>
 #include <unistd.h>
 
 #define PORT 2112
@@ -9,7 +9,7 @@
 int main() {
     printf("Uruchamiam serwer na %s:%d\n", IP, PORT);
 
-    struct sockaddr_in ser,cli;
+    struct sockaddr_in ser, cli;
     int gniazdo, gniazdo2, dlugosc = sizeof cli;
     ssize_t status;
     char buf[200];
@@ -19,34 +19,56 @@ int main() {
     ser.sin_addr.s_addr = inet_addr(IP);
 
     gniazdo = socket(AF_INET, SOCK_STREAM, 0);
-    if (gniazdo == -1) { printf("blad socket\n"); return 0; }
+    if (gniazdo == -1) {
+        printf("Błąd socket\n");
+        return 0;
+    }
 
     status = bind(gniazdo, (struct sockaddr *) &ser, sizeof ser);
-    if (status == -1) { printf("blad bind\n"); return 0; }
-
-    status = listen(gniazdo,10);
-    if (status == -1) { printf("blad listen\n"); return 0; }
-
-    printf("Oczekuje na polaczenie...\n");
-    gniazdo2 = accept(gniazdo, (struct sockaddr*) &cli, (socklen_t *) &dlugosc);
-    if (gniazdo2 == -2) { printf("blad accept\n"); return 0; }
-
-    while (1){
-        printf("Czekam na wiadomosc...\n");
-        status = recv(gniazdo2, buf, sizeof(buf) - 1, 0);
-        if (status < 0) { printf("blad otrzymywania wiadomosci\n"); return 0; }
-        buf[status] = '\0';
-        printf("OTRZYMANO: #%s#\n", buf);
-        if (strcmp(buf, "Q") == 0) break;
-
-        printf("Podaj wiadomosc do wyslania:");
-        scanf("%s", buf);
-        status = send(gniazdo2, buf, strlen(buf), 0);
-        if (status < 0) { printf("blad wysylania wiadomosci\n"); return 0; }
-        if (strcmp(buf, "Q") == 0) break;
+    if (status == -1) {
+        printf("Błąd bind\n");
+        return 0;
     }
-    printf("Rozlaczam gniazda\n");
+
+    status = listen(gniazdo, 10);
+    if (status == -1) {
+        printf("Błąd listen\n");
+        return 0;
+    }
+
+    printf("Oczekuję na połączenie...\n");
+    while ((gniazdo2 = accept(gniazdo, (struct sockaddr *) &cli, (socklen_t *) &dlugosc)) >= 0) {
+        if (gniazdo2 < 0) {
+            printf("Błąd accept\n");
+            return 0;
+        }
+        printf("Nowy klient podłączył się\n");
+
+        while (1) {
+            printf("Czekam na wiadomość...\n");
+            status = recv(gniazdo2, buf, sizeof(buf) - 1, 0);
+            if (status <= 0) {
+                printf("Błąd otrzymywania wiadomości\n");
+                break;
+            }
+            buf[status] = '\0';
+            printf("OTRZYMANO: #%s#\n", buf);
+            if (strcmp(buf, "Q") == 0) break;
+
+            printf("Podaj wiadomość do wysłania:");
+            scanf("%s", buf);
+            status = send(gniazdo2, buf, strlen(buf), 0);
+            if (status <= 0) {
+                printf("Błąd wysyłania wiadomości\n");
+                break;
+            }
+            if (strcmp(buf, "Q") == 0) break;
+        }
+        printf("Klient rozłączył się\n");
+        close(gniazdo2);
+        printf("Oczekuję na połączenie...\n");
+    }
+    printf("Rozłączam gniazdo główne\n");
     close(gniazdo);
-    close(gniazdo2);
 }
 
