5662bb16541f2c5ac0753fb6aa9c04ec2ddf3574
rubikon02 <rubikon02@gmail.com>
2022-12-19 21:34:31 +0100

    File read/save server/client

--- 9/2_przesylanie_plikow/klient/klient.c
+++ 9/2_przesylanie_plikow/klient/klient.c
@@ -3,7 +3,7 @@
 #include<string.h>
 #include <unistd.h>
 
-#define PORT 2115
+#define PORT 2114
 #define IP "127.0.0.23"
 
 int main() {
@@ -13,34 +13,53 @@ int main() {
     ssize_t status;
     struct sockaddr_in ser;
     char buf[1024];
+    char fileName[1024];
 
     ser.sin_family = AF_INET;
     ser.sin_port = htons(PORT);
     ser.sin_addr.s_addr = inet_addr(IP);
 
     gniazdo = socket(AF_INET, SOCK_STREAM, 0);
-    if (gniazdo==-1) { printf("blad socket\n"); return 0; }
+    if (gniazdo == -1) {
+        printf("blad socket\n");
+        return 0;
+    }
 
-    status = connect(gniazdo, (struct sockaddr*)&ser, sizeof ser);
-    if (status<0) { printf("Nie znaleziono takiego serwera\n"); return 0; }
+    status = connect(gniazdo, (struct sockaddr *) &ser, sizeof ser);
+    if (status < 0) {
+        printf("Nie znaleziono takiego serwera\n");
+        return 0;
+    }
     printf("Polaczono z serwerem\n");
 
     while (1) {
         printf("Podaj nazwe pliku ktory chcesz otrzymac:");
         scanf("%s", buf);
+        strcpy(fileName, buf);
         status = send(gniazdo, buf, strlen(buf), 0);
-        if (status < 0) { printf("blad wysylania wiadomosci\n"); return 0; }
+        if (status < 0) {
+            printf("blad wysylania wiadomosci\n");
+            return 0;
+        }
         if (strcmp(buf, "Q") == 0) break;
 
         printf("Czekam na plik...\n");
         status = recv(gniazdo, buf, (sizeof buf) - 1, 0);
-        if (status < 0) { printf("blad otrzymywania wiadomosci\n"); return 0; }
-        buf[status]='\0';
+        if (status < 0) {
+            printf("blad otrzymywania wiadomosci\n");
+            return 0;
+        }
+        buf[status] = '\0';
         if (strcmp(buf, "Q") == 0) break;
-        if (strcmp(buf))
+//        if (strcmp(buf))
         printf("----- Otrzymano plik -----\n");
         printf("%s\n", buf);
         printf("------ Koniec pliku ------\n");
+        FILE *file = fopen(fileName, "w");
+        size_t savedBytes = fwrite(buf, 1, status, file);
+        fclose(file);
+        printf("Zapisano %zu znaków do pliku %s\n", savedBytes, fileName);
+
     }
     printf("Rozlaczam gniazdo\n");
     close(gniazdo);
--- 9/2_przesylanie_plikow/serwer/serwer.c
+++ 9/2_przesylanie_plikow/serwer/serwer.c
@@ -3,13 +3,13 @@
 #include<string.h>
 #include <unistd.h>
 
-#define PORT 2115
+#define PORT 2114
 #define IP "127.0.0.23"
 
 int main() {
     printf("Uruchamiam serwer na %s:%d\n", IP, PORT);
 
-    struct sockaddr_in ser,cli;
+    struct sockaddr_in ser, cli;
     int gniazdo, gniazdo2, dlugosc = sizeof cli;
     ssize_t status;
     char buf[1024];
@@ -19,23 +19,38 @@ int main() {
     ser.sin_addr.s_addr = inet_addr(IP);
 
     gniazdo = socket(AF_INET, SOCK_STREAM, 0);
-    if (gniazdo == -1) { printf("blad socket\n"); return 0; }
+    if (gniazdo == -1) {
+        printf("blad socket\n");
+        return 0;
+    }
 
     status = bind(gniazdo, (struct sockaddr *) &ser, sizeof ser);
-    if (status == -1) { printf("Nie mozna przypisac portu %d\n", PORT); return 0; }
+    if (status == -1) {
+        printf("Nie mozna przypisac portu %d\n", PORT);
+        return 0;
+    }
 
-    status = listen(gniazdo,10);
-    if (status == -1) { printf("blad listen\n"); return 0; }
+    status = listen(gniazdo, 10);
+    if (status == -1) {
+        printf("blad listen\n");
+        return 0;
+    }
 
     printf("Oczekuje na polaczenie...\n");
-    gniazdo2 = accept(gniazdo, (struct sockaddr*) &cli, (socklen_t *) &dlugosc);
-    if (gniazdo2 == -2) { printf("blad accept\n"); return 0; }
+    gniazdo2 = accept(gniazdo, (struct sockaddr *) &cli, (socklen_t *) &dlugosc);
+    if (gniazdo2 == -2) {
+        printf("blad accept\n");
+        return 0;
+    }
     printf("Klient podlaczyl sie\n");
 
-    while (1){
+    while (1) {
         printf("Czekam na wiadomosc...\n");
         status = recv(gniazdo2, buf, sizeof(buf) - 1, 0);
-        if (status < 0) { printf("blad otrzymywania wiadomosci\n"); return 0; }
+        if (status < 0) {
+            printf("blad otrzymywania wiadomosci\n");
+            return 0;
+        }
         buf[status] = '\0';
         printf("OTRZYMANO: #%s#\n", buf);
         if (strcmp(buf, "Q") == 0) break;
@@ -47,12 +62,16 @@ int main() {
             strcpy(buf, "Nie ma takiego pliku");
         } else {
             size_t recivedBytes = fread(buf, 1, sizeof buf - 1, file);
+            fclose(file);
             buf[recivedBytes] = '\0';
             printf("%s\n", buf);
             printf("Wysylam plik\n");
         }
         status = send(gniazdo2, buf, strlen(buf), 0);
-        if (status < 0) { printf("blad wysylania wiadomosci\n"); return 0; }
+        if (status < 0) {
+            printf("blad wysylania wiadomosci\n");
+            return 0;
+        }
     }
     printf("Rozlaczam gniazda\n");
     close(gniazdo);
